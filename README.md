4e799f9751e1fb2689f8466f0749e2be15e07b93
eunsu29 <edit1643@gmail.com>
2020-12-04 21:43:15 +0900

    Update detail of pages

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # EunTube
 
 Cloning Youtube with VanillaJS and NodeJS
+
+## Pages:
+
+- [ ] Home
+- [x] Join
+- [x] Login
+- [x] Search
+- [ ] User Detail
+- [ ] Edit Profile
+- [ ] Change Password
+- [ ] Upload Videos
+- [ ] Video Detail
+- [ ] Edit Video
---
+++ db.js
@@ -0,0 +1,40 @@
+export const videos = [
+  {
+    id: 324383,
+    title: "Video awesome",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "https://www.youtube.com/embed/sRkXQDIommw",
+    creator: {
+      id: 121212,
+      name: "Nicolas",
+      email: "nico@gmail.com",
+    },
+  },
+  {
+    id: 121212,
+    title: "Video Good",
+    description: "This is something I love",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 121212,
+      name: "Nicolas",
+      email: "nico@gmail.com",
+    },
+  },
+  {
+    id: 1112312,
+    title: "Video nice",
+    description: "This is something I love",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 121212,
+      name: "Nicolas",
+      email: "nico@gmail.com",
+    },
+  },
+];
