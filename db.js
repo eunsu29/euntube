d23a6d8b3c018ff0752452481e8fb5a4026443f3
eunsu29 <edit1643@gmail.com>
2020-12-08 02:08:49 +0900

    Upload, Edit, Delete Video Function

--- db.js
+++ db.js
@@ -1,40 +1,16 @@
-export const videos = [
-  {
-    id: 324383,
-    title: "Video awesome",
-    description: "This is something I love",
-    views: 24,
-    videoFile: "https://www.youtube.com/embed/sRkXQDIommw",
-    creator: {
-      id: 121212,
-      name: "Nicolas",
-      email: "nico@gmail.com",
-    },
-  },
-  {
-    id: 121212,
-    title: "Video Good",
-    description: "This is something I love",
-    views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 121212,
-      name: "Nicolas",
-      email: "nico@gmail.com",
-    },
-  },
-  {
-    id: 1112312,
-    title: "Video nice",
-    description: "This is something I love",
-    views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 121212,
-      name: "Nicolas",
-      email: "nico@gmail.com",
-    },
-  },
-];
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("✅ Connected to DB");
+const handleError = () => console.log(`❌ Error on DB Connection:${error}`);
+
+db.once("open", handleOpen);
+db.on("error", handleError);
