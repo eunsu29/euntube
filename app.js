14ea5d7c8116c3a6807161f42874e903c312d021
eunsu29 <edit1643@gmail.com>
2020-12-15 01:11:56 +0900

    Update Login Function

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 import morgan from "morgan";
 import helmet from "helmet";
+import session from "express-session";
+import passport from "passport";
 import { localsMiddleware } from "./middlewares";
-import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
-import routes from "./routes";
+import globalRouter from "./routers/globalRouter";
+
+import "./passport";
 
 const app = express();
 
@@ -19,6 +23,16 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
+app.use(
+  session({
+    secret: process.env.COOKIE_SECRET,
+    resave: true,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(localsMiddleware);
 app.use((req, res, next) => {
   res.setHeader(
